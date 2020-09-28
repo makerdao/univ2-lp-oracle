66c35bb98d896f5bc796fa6f226cf64b98be2f46
Lucas <lucas@makerdao.com>
2020-09-28 11:52:33 -0400

    fix: update poolName to ETH-DAI

--- src/Univ2LpOracle.t.sol
+++ src/Univ2LpOracle.t.sol
@@ -19,9 +19,9 @@ contract UNIV2LPOracleTest is DSTest {
     
     address constant ETH_DAI_UNI_POOL = 0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11;
     address constant ETH_ORACLE       = 0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763;
-    address constant USDC_ORACLE      = 0x77b68899b99b686F415d074278a9a16b336085A0;
+    address constant USDC_ORACLE      = 0x77b68899b99b686F415d074278a9a16b336085A0; // Using in place for DAI price
     
-    bytes32 poolName = "ETH-USDC-UNIV2-LP";
+    bytes32 poolName = "ETH-DAI-UNIV2-LP";
 
     function setUp() public {
         hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
