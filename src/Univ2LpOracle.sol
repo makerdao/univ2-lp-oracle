dcdb2743fb58c722611c73923fc102ddbdb4d4ca
NiklasKunkel <Niklas@theKunkels.com>
2020-10-09 23:13:14 -0700

    added support for pools containing non-standard decimals

--- src/Univ2LpOracle.sol
+++ src/Univ2LpOracle.sol
@@ -15,37 +15,45 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-//How to Calculate LP Token Price:
-//
+///////////////////////////////////////////////////////
+//                                                   //
+//    Methodology for caclulating LP Token Price     //
+//                                                   //
+///////////////////////////////////////////////////////
+
 // INVARIANT k = reserve0 [num token0] * reserve1 [num token1] //need to take into account decimals of LP component tokens
 //
 // k = r_x * r_y
 // r_y = k / r_x
 //
 // 50-50- pools try to stay balanced in dollar terms
-// r_x * p_x = r_y * p_y    //the proportion of r_x and r_y can be manipulated so need to calc them
+// r_x * p_x = r_y * p_y    //the proportion of r_x and r_y can be manipulated so need to normalize them
 //
 // r_x * p_x = p_y (k / r_x)
 // r_x^2 = k * p_y / p_x
 // r_x = sqrt(k * p_y / p_x) & r_y = sqrt(k * p_x / p_y)
-// ^^^whats cool about this equation is that we can get the price ratio of p_y / p_x through priceCumulativeList
-// (this is essentially Uniswap's Oracle)
-// price0CumulativeLast is the price of token x denominated in token y
-// price1CumulativeLast is the price of token y denominated in token x
-// to convert price#CumulativeLast into a usable number we need to take 2 reference points at different times.
-// p_x / p_y = (price0CumulativeLatest_2 - price0CumulativeLatest_1) / (t2 - t1)
-// 
+//
 // now that we've calculated normalized values of r_x and r_y that are not prone to manipulation by an attacker,
 // we can calculate the price of an lp token using the following formula. 
 //
 // p_lp = (r_x * p_x + r_y * p_y) / supply_lp
 //
+// [OPTIONAL] Maker Oracles vs Uniswap Oracles
+// Note the Uniswap Oracle price is the TWAP over the interval t2 - t1 (in our case 1 hour)
+// This means when the price volatility > 2% the calculated price could be quite inaccurate.
+// It is better to use the MakerDAO Medianizer price as it updates on a 0.5%/1% spread
+// It's "safe" to use the Medianizer value because the `cur` price undergoes the OSM delay `hop`
+// Nonetheless for completeness below is a manner of utilizing the Uniswap Oracle.
 //
-// Alternatively this is what someone on the Maker Forum came up with for V1 which looks very similar.
-//  BAL_ETH = SQRT( k / ETHUSD)
-//  BAL_DAI = SQRT( k / (1 / ETHUSD))          // (USDETH === 1 / ETHUSD)
-//  LP Share (USD) = (BAL_ETH * ETHUSD) + (BAL_DAI * 1)
-//  LP Price (USD) = LP Share / LP Token Supply
+// whats cool about the equation r_x = sqrt(k * p_y / p_x)  & r_y = sqrt(k * p_x / p_y)
+// is that we can get the price ratio of p_y / p_x and p_x / p_y through priceCumulativeList
+// (this is essentially Uniswap's Oracle)
+// price0CumulativeLast is the price of token x denominated in token y
+// price1CumulativeLast is the price of token y denominated in token x
+// to convert price#CumulativeLast into a usable number we need to take 2 reference points at different times.
+// p_x / p_y = (priceXCumulativeLatest_2 - priceXCumulativeLatest_1) / (t2 - t1)
+// this ratio can then be used to calculate the normalized reserves
+// ultimately for pools where neither component is pegged to USD a single external Oracle would still be necessary
 
 pragma solidity ^0.5.12;
 
@@ -70,7 +78,7 @@ interface OracleLike {
     function peek() external view returns (uint256,bool);
 }
 
-//Factory for producting UNIV2LPOracle instances
+//Factory for creating UNIV2LPOracle instances
 contract UNIV2LPOracleFactory {
 
     // --- Auth ---
@@ -128,6 +136,9 @@ contract UNIV2LPOracle {
     function add(uint x, uint y) internal pure returns (uint z) {
         require((z = x + y) >= x, "ds-math-add-overflow");
     }
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x, "ds-math-sub-underflow");
+    }
     function mul(uint x, uint y) internal pure returns (uint z) {
         require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
     }
@@ -161,8 +172,8 @@ contract UNIV2LPOracle {
     uint16     constant ONE_HOUR = uint16(3600);
     uint16     public  hop = ONE_HOUR;  //minimum time inbetween price updates
 
-    uint8      public  token0Decimals;  //decimals of token0
-    uint8      public  token1Decimals;  //decimals of token1
+    uint8      public  token0Decimals = uint8(1);  //decimals of token0
+    uint8      public  token1Decimals = uint8(1);  //decimals of token1
 
      struct Feed {
         uint128 val;    //price
@@ -216,35 +227,46 @@ contract UNIV2LPOracle {
 
         (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) = UniswapV2PairLike(src).getReserves();  //pull reserves
         require(_blockTimestampLast == block.timestamp);
+        emit Debug(0, _reserve0);
+        emit Debug(1, _reserve1);
 
-        emit Debug(11, _reserve0);
-        emit Debug(12, _reserve1);
+        // adjust reserves w/ respect to decimals 
+        if (token0Decimals != uint8(18)) {
+            _reserve0 = uint112(_reserve0 * 10 ** sub(18, token0Decimals));
+        }
+        if (token1Decimals != uint8(18)) {
+            _reserve1 = uint112(_reserve1 * 10 ** sub(18, token1Decimals));
+        }
+        
+        emit Debug(10, _reserve0);
+        emit Debug(11, _reserve1);
+
         uint k = mul(_reserve0, _reserve1);                 // Calculate constant product invariant k (WAD * WAD)
-        emit Debug(1, k);
+        emit Debug(2, k);
 
         // All Oracle prices are priced with 18 decimals against USD
         uint token0Price = OracleLike(token0Oracle).read(); // Query token0 price from oracle (WAD)
-        emit Debug(2, token0Price);
+        emit Debug(3, token0Price);
         uint token1Price = OracleLike(token1Oracle).read(); // Query token1 price from oracle (WAD)
-        emit Debug(3, token1Price);
+        emit Debug(4, token1Price);
 
         // TODO: Use priceCumulativeLast in place of p_y / p_x from external oracles for better accuracy when calculating balances
         // formula: (py / px) = (priceCumulativeLast2 - priceCumulativeLast1) / (t2 - t1)
         //^^^ this requires we track priceCumulativeLast in storage for future ref point
-        emit Debug(4, wdiv(token1Price, token0Price));
-        uint balToken0 = sqrt(wmul(k, wdiv(token1Price, token0Price)));  // Get token0 balance (WAD)
-        emit Debug(5, balToken0);
-        uint balToken1 = wdiv(k, balToken0) / WAD;                       // Get token1 balance; gas-savings
-        emit Debug(6, balToken1);
+        emit Debug(5, wdiv(token1Price, token0Price));
+        uint normReserve0 = sqrt(wmul(k, wdiv(token1Price, token0Price)));  // Get token0 balance (WAD)
+        emit Debug(20, normReserve0);
+        uint normReserve1 = wdiv(k, normReserve0) / WAD;                    // Get token1 balance; gas-savings
+        emit Debug(21, normReserve1);
 
-        uint lpTokenSupply = ERC20Like(src).totalSupply();               // Get LP token supply
-        emit Debug(7, lpTokenSupply);
+        uint lpTokenSupply = ERC20Like(src).totalSupply();                  // Get LP token supply
+        emit Debug(8, lpTokenSupply);
 
         lpTokenPrice_ = uint128(
             wdiv(
                 add(
-                    wmul(balToken0, token0Price), // (WAD)
-                    wmul(balToken1, token1Price)  // (WAD)
+                    wmul(normReserve0, token0Price), // (WAD)
+                    wmul(normReserve1, token1Price)  // (WAD)
                 ), 
                 lpTokenSupply // (WAD)
             )
--- src/Univ2LpOracle.t.sol
+++ src/Univ2LpOracle.t.sol
@@ -15,36 +15,66 @@ interface OSMLike {
 
 contract UNIV2LPOracleTest is DSTest {
     Hevm          hevm;
-    UNIV2LPOracle oracle;
+    UNIV2LPOracle ethDaiLPOracle;
+    UNIV2LPOracle ethUsdcLPOracle;
     
     address constant ETH_DAI_UNI_POOL = 0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11;
     address constant ETH_ORACLE       = 0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763;
     address constant USDC_ORACLE      = 0x77b68899b99b686F415d074278a9a16b336085A0; // Using in place for DAI price
+
+    address constant ETH_USDC_UNI_POOL = 0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc;
     
-    bytes32 poolName = "ETH-DAI-UNIV2-LP";
+    bytes32 poolNameDAI = "ETH-DAI-UNIV2-LP";
+    bytes32 poolNameUSDC = "ETH-USDC-UNIV2-LP";
 
     function setUp() public {
         hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
-        oracle = new UNIV2LPOracle(ETH_DAI_UNI_POOL, poolName, ETH_ORACLE, USDC_ORACLE);
+        ethDaiLPOracle = new UNIV2LPOracle(ETH_DAI_UNI_POOL, poolNameDAI, ETH_ORACLE, USDC_ORACLE);
+        ethUsdcLPOracle = new UNIV2LPOracle(ETH_USDC_UNI_POOL, poolNameUSDC, ETH_ORACLE, USDC_ORACLE);
 
+        //whitelist ethDaiLP on ETH Oracle
         hevm.store(
             address(ETH_ORACLE),
-            keccak256(abi.encode(address(oracle), uint256(5))), // Whitelist oracle
+            keccak256(abi.encode(address(ethDaiLPOracle), uint256(5))), // Whitelist oracle
             bytes32(uint256(1))
         );
     }
 
-    // function testFail_basic_sanity() public {
-    //     assertTrue(false);
-    // }
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
 
-    // function test_basic_sanity() public {
-    //     assertTrue(true);
-    // }
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
 
-    function test_seek() public {
-        (uint128 lpTokenPrice, uint32 zzz) = oracle.seek();
+     function test_constructor() public {
+        assertEq(ethDaiLPOracle.src(), ETH_DAI_UNI_POOL);
+        assertEq(ethDaiLPOracle.token0Oracle(), ETH_ORACLE);
+        assertEq(ethDaiLPOracle.token1Oracle(), USDC_ORACLE);
+        assertEq(ethDaiLPOracle.wards(address(this)), 1);
+        assertEq(ethDaiLPOracle.stopped(), 0);
+    }
+
+    function test_seek_dai() public {
+        (uint128 lpTokenPrice, uint32 zzz) = ethDaiLPOracle.seek();
         assertEq(uint256(lpTokenPrice), 1);
+    }
+
+    function test_seek_usdc() public {
+        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        hevm.store(
+            address(ETH_ORACLE),
+            keccak256(abi.encode(address(ethUsdcLPOracle), uint256(5))), // Whitelist oracle
+            bytes32(uint256(1))
+        );
+
+        (uint128 lpTokenPrice, uint32 zzz) = ethUsdcLPOracle.seek();
+        assertEq(uint256(lpTokenPrice), 1);
+    }
+
+    function test_poke() public { 
+        ethDaiLPOracle.poke();
     }
 }
